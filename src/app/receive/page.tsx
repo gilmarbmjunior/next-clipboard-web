0fee0fd693ce175632c23707d97c7bd1d64ae5b3
Gilmar Júnior <gilmarjunior@outlook.com>
2026-08-15 10:51:00 -0300

    20260815-1050

--- src/app/receive/page.tsx
+++ src/app/receive/page.tsx
@@ -10,7 +10,7 @@ export default function Receive() {
     <div className="w-full h-full flex flex-col p-4 gap-4">
       <div className="flex flex-row flex-wrap gap-x-4 gap-y-2">
         <span className="mr-auto">Área de Transferência Compartilhada</span>
-        <div className="hidden flex-row gap-4">
+        <div className="flex flex-row gap-4">
           <span>PIN</span>
           <input type="text" placeholder="000000" className="border"/>
         </div>
