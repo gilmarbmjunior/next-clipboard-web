ebcf289bf899043485b9c26267eca3638dad5bc7
Gilmar Júnior <gilmarjunior@outlook.com>
2026-08-15 10:14:56 -0300

    20260815-1014

--- src/app/receive/page.tsx
+++ src/app/receive/page.tsx
@@ -7,7 +7,13 @@ export default function Receive() {
 
   return (
     <div className="w-full h-full flex flex-col p-4 gap-4">
-      <span>Área de Transferência Compartilhada</span>
+      <div className="flex flex-row flex-wrap gap-x-4 gap-y-2">
+        <span className="mr-auto">Área de Transferência Compartilhada</span>
+        <div className="hidden flex-row gap-4">
+          <span>PIN</span>
+          <input type="text" placeholder="000000" className="border"/>
+        </div>
+      </div>
       <textarea className="flex-1 resize-none border border-gray-500 focus:outline-none p-4 text-gray-500" readOnly value={text} onChange={(e) => setText(e.target.value)} placeholder="O texto compartilhado aparece aqui."></textarea>
     </div>
   );
--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -7,7 +7,13 @@ export default function Send() {
 
   return (
     <div className="w-full h-full flex flex-col p-4 gap-4">
-      <span>Área de Transferência Compartilhada</span>
+      <div className="flex flex-row flex-wrap gap-x-4 gap-y-2">
+        <span className="mr-auto">Área de Transferência Compartilhada</span>
+        <div className="hidden flex-row gap-4">
+          <span>PIN</span>
+          <input type="text" placeholder="000000" className="border"/>
+        </div>
+      </div>
       <textarea className="flex-1 resize-none border border-gray-500 focus:outline-none p-4 text-gray-500" value={text} onChange={(e) => setText(e.target.value)} placeholder="Insira o texto a ser compartilhado aqui e clique em enviar."></textarea>
       <button className="border border-gray-300 hover:bg-gray-200 bg-gray-100 active:bg-gray-300" onClick={send} disabled={sending}>Enviar</button>
     </div>
